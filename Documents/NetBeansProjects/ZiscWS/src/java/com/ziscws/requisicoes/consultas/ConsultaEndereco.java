1729d49b5541f05f92bd854d2afe1f9a6adf711b
AVNT-3A-NB01\Avanti Premium <marcosbenevides@outlook.com.br>
2017-09-20 18:27:00 -0300

    Implementado no ConsultaEndereco um teste para captura dos dados do usuario, que será utilizado no LogLogin. --> captura de ip --> captura de device

--- Documents/NetBeansProjects/ZiscWS/src/java/com/ziscws/requisicoes/consultas/ConsultaEndereco.java
+++ Documents/NetBeansProjects/ZiscWS/src/java/com/ziscws/requisicoes/consultas/ConsultaEndereco.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license ipAdress, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -7,12 +7,18 @@ package com.ziscws.requisicoes.consultas;
 
 import com.ziscws.dao.EnderecoDAO;
 import com.ziscws.dao.UsuarioDAO;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -31,6 +37,18 @@ public class ConsultaEndereco {
         
         return dao.buscaEnderecoUsuario(udao.buscaUsuario(new Long(idusuario)));
         
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String test(@Context HttpServletRequest httpheader){
+        
+        String ipAdress = httpheader.getRemoteAddr();
+        String tipo = httpheader.getHeader("User-Agent");
+        return "<html><body> "
+                + "<p>IP ->" + ipAdress + 
+                " <p>User-Agent ->" + tipo
+                + "</body></html>";
     }
     
 }
